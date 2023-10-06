9835948b54e0d7cbcb32290498803d96502aeb4a
katiegoines <katiegoines@gmail.com>
2023-10-05 17:37:15 -0700

    testingfilter

--- .github/workflows/scripts/check_bundle_size.js
+++ .github/workflows/scripts/check_bundle_size.js
@@ -1,16 +1,63 @@
 module.exports = {
-  checkBundleSize: ({ github, context, core }) => {
-    const {
-      issue: { number: issue_number },
-      repo: { owner, repo }
-    } = context;
-
+  checkBaseBundleSize: () => {
     const fs = require('fs');
     const jsonString = fs.readFileSync('.github/analyze/bundles.json');
     const data = JSON.parse(jsonString);
-    const filtered = data.pages.filter((page) => {
-      console.log(page);
+    const pagesToCheck = [
+      '/',
+      '/cli/start/install',
+      '/lib/auth/getting-started/q/platform/[platform]',
+      '/start',
+      '/cli'
+    ];
+    const baseBundleSizes = [];
+    data.pages.filter((page) => {
+      if (pagesToCheck.includes(page.label)) {
+        baseBundleSizes.push({
+          page: page.label,
+          gzipSize: page.gzipSize,
+          parsedSize: page.parsedSize,
+          statSize: page.statSize,
+          totalGzipSize: page.totalGzipSize,
+          totalParsedSize: page.totalParsedSize,
+          totalStatSize: page.totalStatSize
+        });
+      }
     });
-    // console.log(filtered);
+    return baseBundleSizes;
+  },
+
+  checkHeadBundleSize: () => {
+    const fs = require('fs');
+    const jsonString = fs.readFileSync('.github/analyze/bundles.json');
+    const data = JSON.parse(jsonString);
+    const pagesToCheck = [
+      '/',
+      '/cli/start/install',
+      '/lib/auth/getting-started/q/platform/[platform]',
+      '/start',
+      '/cli'
+    ];
+    const headBundleSizes = [];
+    data.pages.filter((page) => {
+      if (pagesToCheck.includes(page.label)) {
+        baseBundleSizes.push({
+          page: page.label,
+          gzipSize: page.gzipSize,
+          parsedSize: page.parsedSize,
+          statSize: page.statSize,
+          totalGzipSize: page.totalGzipSize,
+          totalParsedSize: page.totalParsedSize,
+          totalStatSize: page.totalStatSize
+        });
+      }
+    });
+    return headBundleSizes;
+  },
+
+  compareBundles: (baseBundleSizes, headBundleSizes) => {
+    console.log(baseBundleSizes, headBundleSizes);
   }
 };
+
+// get base bundle sizes, get pr bundle sizes, compare, fail if difference is +1.05%
